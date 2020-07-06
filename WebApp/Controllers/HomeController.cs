5133d2cbbbb3d1fe5aab9337b628170b27d9e31f
Astr0nAT <astron.autnr@gmail.com>
2020-07-06 16:39:53 +0200

    Added old files

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Impressum()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class User
+    {
+        private double _height;
+
+        // automatische Properties
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Password2 { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        // normales Property
+
+        public double Height
+        {
+            get
+            {
+                return this._height;
+            }
+
+            set
+            {
+                // Value entspricht dem Parameter der set-Methode
+                if(value >= 0.0)
+                {
+                    this._height = value;
+                }
+            }
+        }
+
+        // ctors
+
+        public User() : this("","","", DateTime.MinValue, 0.0) {}
+
+        public User(string firstname, string lastname, string mail, DateTime birthdate, double height)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Email = mail;
+            this.Birthdate = birthdate;
+            this.Height = height;
+            this.Password = "";
+            this.Password2 = "";
+        }
+    }
+}
